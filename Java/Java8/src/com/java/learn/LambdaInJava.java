0e7fccfd69e9cb68d5fa97fc83f9b0ab237bab49
mailsfoss <mailsforss@gmail.com>
2023-04-02 21:16:56 -0400

    Changes for Lambda test

--- Java/Java8/src/com/java/learn/LambdaInJava.java
+++ Java/Java8/src/com/java/learn/LambdaInJava.java
@@ -1,22 +1,71 @@
 package com.java.learn;
 
+import java.util.Random;
+
 public class LambdaInJava {
 
 	public static void main(String[] args) {
 		
-		Calculator addition = (a,b)-> a+b;
 		
-		Calculator subtraction = (a,b)-> a-b;
+		ActorSupplier actor = () ->{
+			String actorName = "";
+			
+			Random randNum = new Random();
+			
+			int a=randNum.nextInt()%3;
+			
+			switch (a) {
+			case 1:
+				actorName = "Sivaji";
+				break;
+			case 2:
+				actorName = "MGR";
+				break;
+			case 0:
+				actorName = "Rajini";
+				break;
+
+			default:
+				 actorName = "someActor";
+				break;
+			}
+			
+			return actorName;
+		};
 		
-		System.out.println("Addition lambda:"+addition.calculate(2, 3));
 		
-		System.out.println("subtraction lambda:"+subtraction.calculate(12, 3));
+		ActorConsumer hero = (heroName) -> {
+			System.out.println("Born as "+heroName);
+			System.out.println(heroName+" works hard");
+			heroName = heroName.concat(" Becomes rolemodel and a ***Hero****");
+			System.out.println(heroName);
+		};
+		
+		hero.act(actor.getName());
+		
+		ActorConsumer villain =(villainName) ->{
+			System.out.println("Born as "+villainName);
+			System.out.println(villainName+" works hard");
+			System.out.println(villainName+" becomes fed up as he does not get what he deserved");
+			System.out.println(villainName+" gets his vengeance atlast");
+			
+		};
+		
+		villain.act(actor.getName());
 
 	}
 	
+	
+	
 	@FunctionalInterface
-	interface Calculator{
-		int calculate(int a, int b);
+	interface ActorConsumer{
+		void act(String role);
 	}
+	
+	@FunctionalInterface
+	interface ActorSupplier{
+		String getName();
+	}
+
 
 }
